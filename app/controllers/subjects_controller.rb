509c4690762f55b6fd41777ccfbe5dc0835c1a5f
Nicholas <nicholasmarques@cjr.org.br>
2018-04-30 23:30:42 -0300

    adicionando materias na tabela

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -5,6 +5,10 @@ class SubjectsController < ApplicationController
   # GET /subjects.json
   def index
     @subjects = Subject.all
+    respond_to do |format|
+      format.html
+      format.json
+    end
   end
 
   # GET /subjects/1
