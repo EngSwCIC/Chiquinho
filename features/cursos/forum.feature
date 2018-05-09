bf57908f7a5ea9845105374f7e8b9760a3633493
Diogo Queiroz <diogoqueirozsantos97@gmai.com>
2018-05-09 10:30:34 -0300

    adicionando os steps de forum

--- features/cursos/forum.feature
+++ features/cursos/forum.feature
@@ -6,7 +6,7 @@ Funcionalidade: Ver um fórum de discussão do curso
   Para que eu obtenha mais informações sobre um curso
   Eu quero poder ver um fórum de discussão sobre o curso
 
-  Delineação do Cenário: Usuário vê um fórum com informações sobre o curso
+  Cenário: Usuário vê um fórum com informações sobre o curso
     Dado que eu esteja na página do curso
     E que exista um link para o fórum do curso
     Quando eu clicar no link "Fórum"
---
+++ features/step_definitions/cursos/forum_steps.rb
@@ -0,0 +1,12 @@
+Dado("que eu esteja na página do curso") do
+    pending # Write code here that turns the phrase above into concrete actions
+end
+  
+Dado("que exista um link para o fórum do curso") do
+    pending # Write code here that turns the phrase above into concrete actions
+end
+  
+Então("eu devo ver uma lista com diferentes tópicos sobre assuntos relacionados ao curso") do
+    pending # Write code here that turns the phrase above into concrete actions
+end
+  
