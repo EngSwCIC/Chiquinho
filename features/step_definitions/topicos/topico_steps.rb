1bb65a8b2a9566510eb0453253517bee60905c01
Diogo Queiroz <diogoqueirozsantos97@gmai.com>
2018-05-23 18:38:02 -0300

    gerando features de topicos

---
+++ features/step_definitions/topicos/topico_steps.rb
@@ -0,0 +1,13 @@
+Quando("preencher o fórmulário com:") do |table|
+    # table is a Cucumber::MultilineArgument::DataTable
+    pending # Write code here that turns the phrase above into concrete actions
+end
+  
+Então("eu devo ser redirecionado para a página do tópico") do
+    pending # Write code here that turns the phrase above into concrete actions
+end
+  
+Então("ver os dados:") do |table|
+    # table is a Cucumber::MultilineArgument::DataTable
+    pending # Write code here that turns the phrase above into concrete actions
+end
