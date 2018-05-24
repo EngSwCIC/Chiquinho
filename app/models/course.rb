982659e54de6d27a29472ee225dd4d3b512d856b
Diogo Queiroz <diogoqueirozsantos97@gmai.com>
2018-05-23 21:04:12 -0300

    refatorando model course

--- app/models/course.rb
+++ app/models/course.rb
@@ -4,7 +4,6 @@ class Course < ApplicationRecord
   has_many :users
   has_many :course_subjects
   has_many :subjects,through: :course_subjects
-  has_and_belongs_to_many :subjects
   has_one :forum
 
   private
