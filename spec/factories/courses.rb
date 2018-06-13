fb1b82370dd05ae9ba855a9fdd1076a70f208cd6
Diogo Queiroz <diogoqueirozsantos97@gmai.com>
2018-06-12 22:18:07 -0300

    testando model user

---
+++ spec/factories/courses.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+    factory :course do
+        kind "Presencial"
+        code 1341
+        name "ENGENHARIA DE COMPUTAÇÃO"
+        turn "Diurno"
+        classification "exatas"
+        opcode 1741
+    end
+end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { FactoryBot.create(:user)}
+  let(:comment) { Comment.new(content: 'teste', user: user)}
+
+  context 'when params are valid' do
+    it 'comment valid' do
+      expect(comment).to be_valid
+    end
+  end
+
+  context 'when params are invalid' do
+    let(:comment) { Comment.new(content: '', user: user)}
+
+    it 'comment is invalid' do
+      expect(comment.valid?).to eq(false)
+    end
+
+    it 'error message' do
+      comment.valid?
+      expect(comment.errors.details[:content][0][:error]).to eq(:blank)
+    end
+  end
 end
