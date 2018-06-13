df3bae7b8a70187cf705b647debd4d0deb19cafc
Diogo Queiroz <diogoqueirozsantos97@gmai.com>
2018-06-13 20:52:46 -0300

    teste para criar um comentário em uma materia

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -3,36 +3,44 @@ require 'rails_helper'
 RSpec.describe CommentsController, type: :controller do
     describe 'POST create' do
         let(:user) { FactoryBot.create(:user)}
-        context 'when params are valid' do
-            before do
-                post :create, params: {comment: comment_params}
-            end
-            let(:comment_params) do
-                { comment:{content: 'testando controller', user: user} }
-            end
-            it 'returns http status 302' do
-                expect(response).to have_http_status(302)
-            end
-            
-            it 'comment exists in database' do
-                Comment.create(comment_params[:comment])
-                expect(Comment.find_by(content: comment_params[:comment][:content], user: user)).to be_truthy
-            end
-        end
-        
-        context 'when params are invalid' do
-            before do
-                post :create, params: {comment: comment_params}
-            end
-            
-            let(:comment_params) do
-                { comment:{content: '', user: user} }
+        let(:subject) { FactoryBot.create(:subject)}
+        context 'subject comments' do
+            context 'when params are valid' do
+                before do
+                    sign_in user
+                    request.env['HTTP_REFERER'] = subject_path(subject)
+                    post :create, params: {comment: comment_params}
+                end
+
+                let(:comment_params) do
+                    {content: 'testando controller', user_id: user.id, subject_id: subject.id}
+                end
+                
+                it 'returns http status 302 adn redirect to subject page' do
+                    expect(response).to have_http_status(302)
+                    expect(response).to redirect_to(subject_path(subject))
+                end
+                
+                it 'comment exists in database' do
+                    @comment = Comment.find_by(content: comment_params[:content],user_id: user.id, subject_id: subject.id)
+                    expect(@comment).not_to be_nil
+                end
             end
             
-            it 'return http status 302' do
-                expect(response).to have_http_status(302)
+            context 'when params are invalid' do
+                before do
+                    post :create, params: {comment: comment_params}
+                end
+                
+                let(:comment_params) do
+                    { comment:{content: '', user: user} }
+                end
+                
+                it 'return http status 302' do
+                    expect(response).to have_http_status(302)
+                end
+                
             end
-            
         end
     end
 end
