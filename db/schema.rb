72382c5153a9ece433930851a5e6f58202df952f
André Laranjeira <andrecaldaslaranjeira@gmail.com>
2018-11-02 19:18:44 -0300

    Feature #62 - Model *Created the migration for this feature. *Updated the database schema and seeds to reflect this feature's migration. *Added model tests for this feature. *Updated the seeds file to clear the database adequately before inserting records.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_02_121248) do
+ActiveRecord::Schema.define(version: 2018_10_27_181147) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -122,6 +122,7 @@ ActiveRecord::Schema.define(version: 2018_07_02_121248) do
     t.datetime "updated_at", null: false
     t.bigint "courses_id"
     t.bigint "departments_id"
+    t.string "office"
     t.index ["courses_id"], name: "index_professors_on_courses_id"
     t.index ["departments_id"], name: "index_professors_on_departments_id"
   end
