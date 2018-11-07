134f16f070f7231e25ba9b8975e933e852686762
moltentheory <gstsrs@gmail.com>
2018-11-07 18:41:12 -0200

    Adicionado botao do forum ao sidebar e modificacoes ao html/css

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -15,6 +15,7 @@ Rails.application.config.assets.precompile += %w( waypoints/lib/jquery.waypoints
 Rails.application.config.assets.precompile += %w( favicon.ico )
 Rails.application.config.assets.precompile += %w( login-style.css fullcalendar/fullcalendar.min.css )
 Rails.application.config.assets.precompile += %w( courses.css )
+Rails.application.config.assets.precompile += %w( topics.css )
 Rails.application.config.assets.precompile += %w( fullcalendar/fullcalendar.min.js detect.js fastclick.js )
 Rails.application.config.assets.precompile += %w( star-rating/star-rating.min.js star-rating/theme.js star-rating/star-rating.min.css star-rating/theme.css)
 # Precompile additional assets.
