4cc5aa47e4c60a4677d716d16ee31bc0de02be28
01oseluiz <joseluizgnogueira@live.com>
2018-11-19 14:28:51 -0200

    Destroy dependence is too much slow, was removed and changed seeds

--- app/models/subject_class.rb
+++ app/models/subject_class.rb
@@ -1,7 +1,7 @@
 class SubjectClass < ApplicationRecord
   belongs_to :subject
   belongs_to :professor
-  has_many :class_schedules, dependent: :destroy
+  has_many :class_schedules
   has_many :class_hours, through: :class_schedules
   has_many :week_days, through: :class_schedules
 end
