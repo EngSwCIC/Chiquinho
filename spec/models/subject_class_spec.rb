07b17c806d1c34a8832d3f0eaf0aa9ab2e2cdb9b
01oseluiz <joseluizgnogueira@live.com>
2018-11-19 16:53:41 -0200

    model spec test, create and update

---
+++ spec/models/subject_class_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.describe SubjectClass, type: :model do
+  describe "Create and update subject class" do
+
+    context "with valid params" do
+      let(:subject) { FactoryBot.create(:subject)}
+
+      before do
+        # TODO create a professor factory
+        professor = Professor.create(name: "Professor")
+        @params = {name: "A",
+                  subject_id: subject.id,
+                  professor_id: professor.id}
+      end
+
+      it "create a subject class" do
+        expect(SubjectClass.create(@params)).to be_valid
+      end
+
+      it "update an existent subject class" do
+        subject_class = SubjectClass.create(@params)
+        subject_class.name = "B"
+        subject_class.save!
+
+        expect(subject_class.name).to eq(SubjectClass.last.name)
+      end
+    end
+
+  end
+end
