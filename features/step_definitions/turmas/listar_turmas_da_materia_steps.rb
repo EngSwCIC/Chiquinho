9c19c8a469ffcb2e94730e3258f73ade8e5b73e2
André Laranjeira <andrecaldaslaranjeira@gmail.com>
2018-11-19 13:27:20 -0200

    Feature #71 - Cucumber description and steps *Wrote the Cucumber description for the feature. *Wrote the step definitions for the Cucumber description of the feature.

--- features/step_definitions/turmas/listar_turmas_da_materia_steps.rb
+++ features/step_definitions/turmas/listar_turmas_da_materia_steps.rb
@@ -1,9 +1,30 @@
 # This is the steps file referred to listar_turmas_da_materia feature
 # Place your code relative to that feature here
 
+Dado("que eu esteja na página dessa matéria") do
+  visit subject_path(@subject.id)
+end
+
+Dado("que existam as turmas {string}, {string}, {string} para essa matéria") do
+  |name, schedules, professor|
+  @professor = Professor.find_or_create_by(name: professor)
+  @class = SubjectClass.create(name: name, subject_id: @subject.id,
+                        professor_id: @professor.id)
+
+  schedules.split(", ").each {
+    |schedule|
+    week_day, class_hour = schedule.split(" - ")
+    @week = WeekDay.find_or_create_by(day: week_day)
+    @hour = ClassHour.find_or_create_by(hour: Time.parse(class_hour))
+    ClassSchedule.create(subject_class_id: @class.id, week_day_id: @week.id,
+                         class_hour_id: @hour.id)
+  }
+
+end
+
 Então("devo ver uma lista contendo as turmas {string}, {string}, {string}") do
   |name, schedule, professor|
-  expect(page).to have_content(nome)
-  expect(page).to have_content(horario)
+  expect(page).to have_content("Turma " + name)
+  expect(page).to have_content(schedule)
   expect(page).to have_content(professor)
 end
