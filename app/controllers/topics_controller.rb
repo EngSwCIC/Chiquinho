3faca373aa7319d120e8116c6c371887ed7df3e8
Douglas Shiro Yokoyama <douglasshiro@hotmail.com>
2018-11-22 01:29:08 -0300

    topics: botao de remocao de topicos criados pelo usuario;

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -7,26 +7,26 @@ class TopicsController < ApplicationController
 
     def create
         @topic = Topic.new(topic_params)
-        
-        user = User.find_by(id: session["warden.user.user.key"][0][0]) 
+
+        user = User.find_by(id: session["warden.user.user.key"][0][0])
         @topic.user_id = user.id
-        
+
         if session.has_key?(:filter_course_id)
-            course = Course.find_by(id: session[:filter_course_id]) 
+            course = Course.find_by(id: session[:filter_course_id])
             @topic.course_id = course.id
         end
         if session.has_key?(:filter_professor_id)
-            professor = Professor.find_by(id: session[:filter_professor_id]) 
+            professor = Professor.find_by(id: session[:filter_professor_id])
             @topic.professor_id = professor.id
         end
         if session.has_key?(:filter_subject_id)
-            subject = Subject.find_by(id: session[:filter_subject_id]) 
+            subject = Subject.find_by(id: session[:filter_subject_id])
             @topic.subject_id = subject.id
         end
 
         if @topic.save
-            redirect_to topics_path( { :course_id => session[:filter_course_id], 
-                :professor_id => session[:filter_professor_id], 
+            redirect_to topics_path( { :course_id => session[:filter_course_id],
+                :professor_id => session[:filter_professor_id],
                 :subject_id => session[:filter_subject_id] } )
         else
             flash[:error] = 'Não foi possível salvar seu tópico'
@@ -34,7 +34,7 @@ class TopicsController < ApplicationController
         end
     end
     def show
-        @user = User.find(session["warden.user.user.key"][0][0]) 
+        @user = User.find(session["warden.user.user.key"][0][0])
         @topic = Topic.find(params[:id])
         @responses = Topic.where(topic_id: @topic.id).order(:created_at)
         #@comment = Comment.new
@@ -49,31 +49,37 @@ class TopicsController < ApplicationController
         session.delete(:filter_professor_id)
         session.delete(:filter_subject_id)
         if params.has_key?(:course_id) || params.has_key?(:professor_id) || params.has_key?(:subject_id)
-            if params.has_key?(:course_id) 
+            if params.has_key?(:course_id)
                 session[:filter_course_id] = params[:course_id]
             end
-            if params.has_key?(:professor_id) 
+            if params.has_key?(:professor_id)
                 session[:filter_professor_id] = params[:professor_id]
             end
-            if params.has_key?(:subject_id) 
+            if params.has_key?(:subject_id)
                 session[:filter_subject_id] = params[:subject_id]
             end
         end
 
 
         @query = []
-        if session.has_key?(:filter_course_id) 
+        if session.has_key?(:filter_course_id)
             @query << ('course_id = ' + session[:filter_course_id])
         end
-        if session.has_key?(:filter_professor_id) 
+        if session.has_key?(:filter_professor_id)
             @query << ('professor_id = ' + session[:filter_professor_id])
         end
-        if session.has_key?(:filter_subject_id) 
+        if session.has_key?(:filter_subject_id)
             @query << ('subject_id = ' + session[:filter_subject_id])
         end
 
         @topics = Topic.where(@query.join(' and '))
 
+    end
+
+    def destroy
+      @topic = Topic.find(params[:id])
+      @topic.update(deleted:true)
+      redirect_back fallback_location: topics_url
     end
 
     private
