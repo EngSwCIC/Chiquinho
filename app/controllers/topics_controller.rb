15958e8eaa6d81c61d07433032e969cb1ae9cb59
moltentheory <gstsrs@gmail.com>
2018-12-06 00:49:54 -0200

    Adicionados comentários para o rdoc

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,6 +1,10 @@
 class TopicsController < ApplicationController
     skip_before_action :verify_authenticity_token
     before_action :authenticate_user!, only: [:new, :create]
+
+    ##
+    # Método para a recuperação de dados para a página de criação de novo post ou resposta.
+    # Determina os filtros (professor, curso e/ou matéria) e se o tópico é um post ou uma resposta.
     def new
         @topic = Topic.new
 
@@ -13,6 +17,10 @@ class TopicsController < ApplicationController
         end
     end
 
+    ##
+    # Ao clicar em enviar na pagina de criação de tópico este método é chamado para tratar os dados enviados pelo usuário e salvar o tópico no banco de dados
+    # Em seguida ocorre um redirecionamento se o tópico foi salvo com sucesso.
+    # Redireciona para a página do tópico principal se é uma resposta ou para a página geral do fórum se for um novo tópico.
     def create
         @topic = Topic.new(topic_params)
         @topic.user_id = find_session_user().id
@@ -35,10 +43,15 @@ class TopicsController < ApplicationController
         end
     end
 
+    ##
+    # Método para a página de edição do post.
+    # Busca na database o post a ser editado.
     def edit
         @topic = Topic.find(params[:id])
     end
 
+    ##
+    # Método para tratamento dos dados editados do post e redicionamento em caso de sucesso na edição
     def update
         @topic = Topic.find(params[:id])
         @topic.title = params[:topic][:title]
@@ -55,7 +68,11 @@ class TopicsController < ApplicationController
         end
     end
 
+    ##
+    # Método para exibição de um post específico.
+    # Procura na database o post, suas respostas e os nomes dos autores dos mesmos para exibição.
     def show
+        # Redefine último post visualizado na sessão como sendo o post atual (para utilização do id no CRUD)
         reassign_last_topic()
 
         @user = find_session_user()
@@ -66,6 +83,9 @@ class TopicsController < ApplicationController
 
     end
 
+    ##
+    # Método para exibição do fórum, exibe os posts que tem as tags vindas de params (curso, professor e/ou matéria).
+    # Armazena as tags na sessão para propagação segura em GET (CRUD).
     def index
         clear_session_filters()
 
@@ -77,6 +97,9 @@ class TopicsController < ApplicationController
 
     end
 
+    ##
+    # Método para remoção de post ou resposta
+    # Ao invés de remover o registro da database, altera o campo deleted para true, dessa forma posts podem ser recuperados eventualmente.
     def destroy
       @topic = Topic.find(params[:id])
       @topic.update(deleted:true)
@@ -86,6 +109,8 @@ class TopicsController < ApplicationController
 
     private
 
+    ##
+    # Retorna o usuário logado
     def find_session_user
         user = nil
         if session.has_key?("warden.user.user.key")
@@ -94,6 +119,8 @@ class TopicsController < ApplicationController
         return user
     end
 
+    ##
+    # Limpa os filtros da sessão
     def clear_session_filters
         session.delete(:last_topic_id)
         session.delete(:filter_course_id)
@@ -101,6 +128,8 @@ class TopicsController < ApplicationController
         session.delete(:filter_subject_id)
     end
 
+    ##
+    # Armazena na sessão os filtros obtidos de params (professor, curso ou matéria)
     def store_filters_in_session_from_params
         ['course', 'professor', 'subject'].each do |filter|
             tag_sym = "#{filter}_id".to_sym
@@ -111,11 +140,15 @@ class TopicsController < ApplicationController
         end
     end
 
+    ##
+    # Re-armazena na sessão o último tópico visto
     def reassign_last_topic
         session.delete(:last_topic_id)
         session[:last_topic_id] = params[:id]
     end
 
+    ##
+    # Constrói a query SQL para busca de posts na database com os filtros existentes na sessão
     def build_filters_query
         query = []
         ['course', 'professor', 'subject'].each do |filter|
@@ -132,6 +165,8 @@ class TopicsController < ApplicationController
         return query
     end
 
+    ##
+    # A partir de uma lista de tópicos recupera da database o nome dos autores destes tópicos e retorna uma hash.
     def get_topics_usernames(topics)
         usernames = Hash.new
         topics.each do |topic|
@@ -144,6 +179,8 @@ class TopicsController < ApplicationController
         return usernames
     end
 
+    ##
+    # Constrói uma hash com os filtros da sessão (professor, matéria e curso) para ser utilizada no redirecionamento
     def build_redirect_hash
         redirect_hash = Hash.new
         ['course', 'professor', 'subject'].each do |filter|
@@ -156,6 +193,9 @@ class TopicsController < ApplicationController
         return redirect_hash
     end
 
+    ##
+    # Atribui os filtros existentes em session ao objeto Topic atual
+    # Busca antes na database os objetos de cada filtro (professor, materia e/ou curso) para checar se o mesmo existe
     def assign_topic_categories_from_session
         ['course', 'professor', 'subject'].each do |filter|
             filter_sym = "filter_#{filter}_id".to_sym
@@ -165,6 +205,8 @@ class TopicsController < ApplicationController
         end
     end
 
+    ##
+    # Função auxiliar que procura na database o nome dos objetos filtrados (professor, curso, matéria) e os concatena em uma string
     def get_filters_string
         str = []
         ['course', 'professor', 'subject'].each do |filter|
@@ -176,6 +218,8 @@ class TopicsController < ApplicationController
         return str.join(" · ")
     end
 
+    ##
+    # Recebe os parâmetros do formulário de criação de tópico
     def topic_params
         params.require(:topic).permit(:title, :description)
     end
