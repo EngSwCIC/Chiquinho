6418681b0cae80cac2bcc82700a3e8e94e670052
André Laranjeira <andrecaldaslaranjeira@gmail.com>
2018-12-07 21:09:43 -0200

    Added sad paths in cucumber for features #62 and #71. (#113)

--- features/step_definitions/professores/ver_sala_professor.rb
+++ features/step_definitions/professores/ver_sala_professor.rb
@@ -2,7 +2,14 @@ Dado("que existam professores com campos {string} e {string}") do |name, office|
   @professor = Professor.create(name: name, office: office)
 end
 
-Então("devo ir pra uma pagina com detalhes sobre o professor {string} com a sala {string}") do |name, room|
-  expect(page).to have_content(name)
-  expect(page).to have_content(room)
+Então("devo ver que a sala do professor é {string}") do |room|
+  expect(page).to have_content("Sala: " + room)
+end
+
+Dado("que existam professores com campos {string} sem o campo sala") do |name|
+  @professor = Professor.create(name: name)
+end
+
+Então("devo ver que a sala do professor não está disponível") do
+  expect(page).to have_content("Sala: Não disponível")
 end
