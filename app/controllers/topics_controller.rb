255572db1df6e36f1831d2fd3e42f128b9daf723
Heitor Belem <heitorbelem11@gmail.com>
2019-06-01 02:55:22 -0300

    Editando tópicos criados por mim

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,6 +1,8 @@
 class TopicsController < ApplicationController
+  
   before_action :authenticate_user!, only: [:new, :create, :destroy]
-  before_action :set_topic, only: [:show]
+  before_action :set_topic, only: [:edit, :update, :destroy]
+
   def new
     @topic = Topic.new
     @forum = Forum.find(params[:forum_id])
@@ -10,8 +12,8 @@ class TopicsController < ApplicationController
 
   def create
     @topic = Topic.new(topic_params)
-    forum = Forum.find_by(id: params[:forum_id])
-    @topic.forum_id = forum.id
+    @forum = Forum.find_by(id: params[:forum_id])
+    @topic.forum_id = @forum.id
     @topic.user_id = current_user.id
     if @topic.save
       redirect_to course_forum_path(params[:course_id], params[:forum_id])
@@ -20,8 +22,25 @@ class TopicsController < ApplicationController
     end
   end
 
+  def edit
+    @forum = Forum.find_by(id: params[:forum_id])
+  end
+
+  def update
+    if @topic.user_id == current_user.id
+      respond_to do |format|
+        if @topic.update(topic_params)
+          format.html { redirect_to course_forum_path(params[:course_id], params[:forum_id]), notice: 'Topic was successfully updated.' }
+          format.json { render :show, status: :ok, location: @topic }
+        else
+          format.html { render :edit }
+          format.json { render json: @topic.errors, status: :unprocessable_entity }
+        end 
+      end
+    end
+  end
+
   def destroy
-    @topic = Topic.find(params[:id])
     if @topic.user_id == current_user.id 
       @topic.destroy
       respond_to do |format|
@@ -37,7 +56,13 @@ class TopicsController < ApplicationController
   end
 
   private
+
   def topic_params
     params.require(:topic).permit(:title, :description)
   end
+
+  def set_topic
+    @topic = Topic.find(params[:id])  
+  end
+
 end
