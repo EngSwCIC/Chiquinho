45c4cee6a099c8dd8afc65705eb9ef49fff56bfc
TheKipmaster <fkipman@hotmail.com>
2019-06-14 20:04:55 -0300

    Implementa teste do método de remover matéria da grade

--- spec/models/schedule_spec.rb
+++ spec/models/schedule_spec.rb
@@ -18,4 +18,20 @@ RSpec.describe Schedule, type: :model do
       expect(schedule).not_to be_valid
     end
   end
+
+  context "given that the user tried to remove a subject from his schedule" do
+    let(:user){ FactoryBot.create(:user) }
+    let(:subject){ FactoryBot.create(:subject) }
+
+    before do
+      schedule.user = user
+      schedule.time_8 << subject.name
+      ScheduleSubject.create(subject: subject, schedule: schedule)
+      schedule.save!
+    end
+    it "should be deleted" do
+      user.schedule.find_and_remove_subject(subject.name)
+      expect(schedule.time_8).to eq([nil])
+    end
+  end
 end
