9305c1902fdf1aedf71cb0c9c255eda6395bd605
hugo <hugorezende96@hotmail.com>
2019-06-14 19:32:18 -0300

    testes de filtrar professor

---
+++ features/step_definitions/professores/filtrar_por_professores.rb
@@ -0,0 +1,20 @@
+Quando("eu clicar no botao de filtro") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Quando("eu prencher com o nome de professor:") do |table|
+  # table is a Cucumber::MultilineArgument::DataTable
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Quando("eu apertar o botão pesquisar") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Então("devo ser redirecionado para a pagina materias") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Então("verificar que as materias estao filtradas pelo professor") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
