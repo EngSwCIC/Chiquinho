e4999a48a8598508f9dd59bdc3fee9dcf77a0c4a
Hugo Rezende <hugorezende96@hotmail.com>
2019-06-26 19:54:55 -0300

    Teste filtrar por professor BDD

--- features/professores/filtrar_por_professores.feature
+++ features/professores/filtrar_por_professores.feature
@@ -9,7 +9,7 @@ Funcionalidade: Filtrar matérias por professor
    Dado que eu esteja na página inicial
    Quando eu clicar no botao de filtro
    E eu prencher com o nome de professor:
-   |name|Joao|
+   |professor|Joao|
    E eu apertar o botão pesquisar
    Então devo ser redirecionado para a pagina materias
    E verificar que as materias estao filtradas pelo professor
--- features/step_definitions/professores/filtrar_por_professores.rb
+++ features/step_definitions/professores/filtrar_por_professores.rb
@@ -1,20 +1,26 @@
 Quando("eu clicar no botao de filtro") do
-  pending # Write code here that turns the phrase above into concrete actions
+  click_button("filter")
+
 end
 
 Quando("eu prencher com o nome de professor:") do |table|
-  # table is a Cucumber::MultilineArgument::DataTable
-  pending # Write code here that turns the phrase above into concrete actions
+  table.rows_hash.each do |field,value|
+  	fill_in field, with: value
+  end
+  
 end
 
 Quando("eu apertar o botão pesquisar") do
-  pending # Write code here that turns the phrase above into concrete actions
+  click_button("Pesquisar")
+  #click_button caso não funcionar click_link				
 end
 
 Então("devo ser redirecionado para a pagina materias") do
-  pending # Write code here that turns the phrase above into concrete actions
+  expect(current_path).to eq("/search_subject")
 end
 
 Então("verificar que as materias estao filtradas pelo professor") do
-  pending # Write code here that turns the phrase above into concrete actions
+  #expect(page).to have_content "CÁLCULO 1"
+  #expect(page).to have_content "GEOLOGIA"
+  #expect(page).to have_content "SEMINÁRIO"
 end
