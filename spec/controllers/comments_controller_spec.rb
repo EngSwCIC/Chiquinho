264a6de0852d934a67abd3799f4d9b23bff7346d
Heitor Belém <heitorbelem11@gmail.com>
2019-06-28 15:58:34 -0300

    Heitor:criar comentário- finalizado (TDD e feature)

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -7,6 +7,50 @@ RSpec.describe CommentsController, type: :controller do
     
     let(:valid_session) { {} }
     
+    describe 'POST create' do
+        let(:user) { FactoryBot.create(:user)}
+        context 'criar comentário no topico' do
+            before do
+                @course = Course.create(:kind=>"Presencial", :code=>"19", :name=>"ADMINISTRAÇÃO", :turn=>"Diurno")
+                @user2 = User.create!(email: "admin@admin.com", first_name: "admin", last_name: "aidmin", matricula: "12/3456789", password: "123456", course: @course)                
+                @forum = Forum.create!(course_id: @course.id)
+                @topic = Topic.create(title: 'Titulo Teste', description: 'Desc Teste', user_id: user.id, forum_id: @forum.id)
+                @comment = Comment.create(content: 'Conteudo', user_id: user.id)
+                @comment2 = Comment.create!(content: 'Conteudo', user_id: @user2.id) 
+            end
+            context 'usuário logado' do
+                before do
+                    sign_in user
+                    request.env['HTTP_REFERER'] = course_forum_topic_path(@course.id, @forum.id, @topic.id)
+                    post :create, params: {comment: comment_params}
+                end
+                let(:comment_params) do
+                    {content: 'testando controller', user_id: user.id, topic_id: @topic.id}
+                end
+                it 'returns http status 302 and redirect to subject page' do
+                    expect(response).to have_http_status(302)
+                    expect(response).to redirect_to(course_forum_topic_path(@course.id, @forum.id, @topic.id))
+                end
+            end
+            
+            context 'usuário não ta logado' do
+                before do
+                    request.env['HTTP_REFERER'] = course_forum_topic_path(@course.id, @forum.id, @topic.id)
+                    post :create, params: {comment: comment_params}
+                end
+                let(:comment_params) do
+                    { comment:{content: 'testando falha', user: user} }
+                end
+                it 'redireciona para página de login' do
+                    expect(response).to have_http_status(302)
+                    expect(response).to redirect_to(new_user_session_path)
+                end
+            end
+        end
+    end
+
+
+
     describe 'DELETE destroy' do 
         let(:user){FactoryBot.create(:user)}
         context 'comentario deletado' do
