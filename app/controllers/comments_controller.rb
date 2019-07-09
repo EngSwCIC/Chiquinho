337bfacd503d34a18bd8b0eec79b87670d4e300a
Heitor Belem <heitorbelem11@gmail.com>
2019-07-09 13:31:54 -0300

    Refatorando comments_controller (finalizado)

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -24,6 +24,11 @@ class CommentsController < ApplicationController
         format.html { redirect_to course_forum_topic_path(params[:course_id],params[:forum_id], params[:topic_id]), notice: 'O comentário foi removido com sucesso.' }
         #format.json { head :no_content }
       end
+    else
+      respond_to do |format|
+        format.html { redirect_to course_forum_topic_path(params[:course_id],params[:forum_id], params[:topic_id]), notice: 'O comentário não foi removido.' }
+        #format.json { head :no_content }
+      end
     end
   end
 
