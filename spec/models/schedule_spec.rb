86c0308d628766fa35778a0973180a54f416cf7d
TheKipmaster <fkipman@hotmail.com>
2019-07-11 15:02:19 -0300

    adiciona testes para o método add_subject

--- spec/models/schedule_spec.rb
+++ spec/models/schedule_spec.rb
@@ -3,44 +3,37 @@ require 'rails_helper'
 RSpec.describe Schedule, type: :model do
   let(:schedule) { Schedule.new(time_8: Array.new(6), time_10: Array.new(6), time_12: Array.new(6), time_14: Array.new(6), time_16: Array.new(6), time_19: Array.new(6), time_21: Array.new(6)) }
 
-  it { expect(schedule).to belong_to(:user) } # Testar relacionamento
+  describe 'with relationships' do
+    it { expect(schedule).to belong_to(:user) } # Testar relacionamento
 
-  context 'with a user' do
-    let(:user){ FactoryBot.create(:user) }
+    context 'with a user' do
+      let(:user){ FactoryBot.create(:user) }
 
-    it 'should be valid' do
-      schedule.user = user
-      expect(schedule).to be_valid
+      it 'be valid' do
+        schedule.user = user
+        expect(schedule).to be_valid
+      end
+    end
+
+    context 'without a user' do
+      it 'not be valid' do
+        expect(schedule).not_to be_valid
+      end
     end
   end
 
-  context 'without a user' do
-    it 'should not be valid' do
-      expect(schedule).not_to be_valid
-    end
-  end
-
-  context 'given that the user tried to remove a subject from his schedule' do
+  describe '#remove_subject' do
     let(:user) { FactoryBot.create(:user) }
     let(:subject) { FactoryBot.create(:subject) }
 
     before do
-      schedule = Schedule.new(
-        time_8: Array.new(6),
-        time_10: Array.new(6),
-        time_12: Array.new(6),
-        time_14: Array.new(6),
-        time_16: Array.new(6),
-        time_19: Array.new(6),
-        time_21: Array.new(6)
-      )
       schedule.user = user
       schedule.time_8 << subject.name
       ScheduleSubject.create(subject: subject, schedule: schedule)
       schedule.save!
     end
 
-    it 'should be deleted' do
+    it 'return empty schedule' do
       user.schedule.find_and_remove_subject(subject.name)
       expect(user.schedule.time_8).to eq([nil, nil, nil, nil, nil, nil, nil])
     end
@@ -82,6 +75,18 @@ RSpec.describe Schedule, type: :model do
       expect(schedule.time_16).to eq(empty_schedule.time_16)
       expect(schedule.time_19).to eq(empty_schedule.time_19)
       expect(schedule.time_21).to eq(empty_schedule.time_21)
+    end
+  end
+
+  describe '#add_subject' do
+    let(:subject) { FactoryBot.create(:subject) }
+
+    before do
+      schedule.add_subject('time_8', 2, subject)
+    end
+
+    it 'return schedule with subject added' do
+      expect(schedule.time_8[2]).to eq(subject.name)
     end
   end
 end
